f66172a2fc80baffa07f2690ad909b9630ae2eb1
Lucas D Vidotto <lucasvidotto3@gmail.com>
2023-08-06 14:15:42 -0300

    mor infos

---
+++ src/Pages/Menu/file.tsx
@@ -0,0 +1,8 @@
+export const File = {
+    context : `Materiais inteligentes (Smart Materials) são materiais capazes 
+    de perceber alterações no ambiente ao qual estão inseridos respondendo 
+    de maneira pré-determinada. Normalmente estas respostas ocorrem de maneira 
+    não óbvia, como, por exemplo, mudança de módulo elástico em uma direção 
+    através de aplicação de campo magnético.
+    `
+}
--- src/Pages/Menu/menu.tsx
+++ src/Pages/Menu/menu.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Logo } from "../../Components/Logo/logo"
 import { NavBar } from "../../Components/navbar"
-import '../../styles/menu.css';
+/* import '../../styles/menu.css';
+ */import '../../styles/testemenu.css';
 import Smart from '../../assets/Logo/Logo.png';
 import VideoCard from './videocard';
+import { File } from './file';
 
 const coverImage = 'http://www.labi.ufscar.br/wp-content/uploads/2017/01/musculos-750x400.png';
 const videoUrl = 'https://www.youtube.com/watch?v=QFePn-bPaWM&t=1s'; // Substitua pelo link correto do vídeo do YouTube
@@ -18,6 +20,11 @@ export function Menu() {
           <div className="info-left">
             <div className="image-logo">
               <img src={Smart} alt="Imagem Smart" />
+            </div>
+            <div className="text-container">
+              <span >{File.context}</span>
+              <span >{File.context}</span>
+              <span >{File.context}</span>
             </div>
           </div>
           <div className="barra-right">
